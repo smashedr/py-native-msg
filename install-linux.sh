a3bc82b171ac0c6aadaeca03eed1737050b25370
Shane <6071159+smashedr@users.noreply.github.com>
2024-05-30 21:08:25 -0700

    Fix Version

--- install-linux.sh
+++ install-linux.sh
@@ -4,33 +4,40 @@ set -e
 
 APP_NAME="com.cssnr.extension.python"
 SOURCE="dist"
-DESTINATION="${APP_NAME}_${GITHUB_REF_NAME:-0.1}"
+
+if [ "${GITHUB_EVENT_NAME}" == "release" ];then
+    VERSION="${GITHUB_REF_NAME}"
+else
+    VERSION="0.1"
+fi
+
+PACKAGE="${APP_NAME}_${VERSION}"
 
 echo "SOURCE: ${SOURCE}"
 ls -lah "${SOURCE}"
-echo "dest: ${DESTINATION}"
+echo "dest: ${PACKAGE}"
 
-chrome="${DESTINATION}/etc/opt/chrome/native-messaging-hosts"
-chromium="${DESTINATION}/etc/chromium/native-messaging-hosts"
-firefox="${DESTINATION}/usr/lib/mozilla/native-messaging-hosts"
+chrome="${PACKAGE}/etc/opt/chrome/native-messaging-hosts"
+chromium="${PACKAGE}/etc/chromium/native-messaging-hosts"
+firefox="${PACKAGE}/usr/lib/mozilla/native-messaging-hosts"
 
-mkdir -p "${DESTINATION}/DEBIAN"
-mkdir -p "${DESTINATION}/opt/${APP_NAME}"
+mkdir -p "${PACKAGE}/DEBIAN"
+mkdir -p "${PACKAGE}/opt/${APP_NAME}"
 mkdir -p "${chrome}"
 mkdir -p "${chromium}"
 mkdir -p "${firefox}"
 
-cp "${SOURCE}/client" "${DESTINATION}/opt/${APP_NAME}/client"
-chmod +x "${DESTINATION}/opt/${APP_NAME}/client"
-touch "${DESTINATION}/opt/${APP_NAME}/log.txt"
+cp "${SOURCE}/client" "${PACKAGE}/opt/${APP_NAME}/client"
+chmod +x "${PACKAGE}/opt/${APP_NAME}/client"
+touch "${PACKAGE}/opt/${APP_NAME}/log.txt"
 
 cp "${SOURCE}/manifest-chrome.json" "${chrome}/${APP_NAME}.json"
 cp "${SOURCE}/manifest-chrome.json" "${chromium}/${APP_NAME}.json"
 cp "${SOURCE}/manifest-firefox.json" "${firefox}/${APP_NAME}.json"
 
-cat <<-EOF > "${DESTINATION}/DEBIAN/control"
+cat <<-EOF > "${PACKAGE}/DEBIAN/control"
 Package: ${APP_NAME}
-Version: ${GITHUB_REF_NAME:-0.1}
+Version: ${VERSION}
 Section: base
 Priority: optional
 Architecture: i386
@@ -39,11 +46,11 @@ Description:  CSSNR Python Native Messaging
  https://github.com/smashedr/python-native-messaging
 EOF
 
-echo "Debian: ${DESTINATION}/DEBIAN/control"
-cat "${DESTINATION}/DEBIAN/control"
+echo "Debian: ${PACKAGE}/DEBIAN/control"
+cat "${PACKAGE}/DEBIAN/control"
 
-echo "Building: ${DESTINATION}"
-dpkg-deb --build "${DESTINATION}"
+echo "Building: ${PACKAGE}"
+dpkg-deb --build "${PACKAGE}"
 
 mkdir out
-mv "${DESTINATION}.deb" "out/install-linux.deb"
+mv "${PACKAGE}.deb" "out/install-linux.deb"
